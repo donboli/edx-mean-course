71b4e41c53e1ecbc8c6a2f7b2309ba38762ab36f
Juan Becker <wolli.becker@gmail.com>
2016-02-16 23:13:11 -0600

    first commit

---
+++ product.js
@@ -0,0 +1,33 @@
+var mongoose = require('mongoose');
+var Category = require('./category');
+var fx = require('./fx');
+
+var productSchema = {
+  name: { type: String, required: true },
+  // Pictures must start with "http://"
+  pictures: [{ type: String, match: /^http:\/\//i }],
+  price: {
+    amount: {
+      type: Number,
+      required: true,
+      set: function(v){
+        this.internal.approximatePriceUSD = v / (fx()[this.price.currency] || 1);
+        return v;
+      }
+    },
+    // Only 3 supported currencies for now
+    currency: {
+      type: String,
+      enum: ['USD', 'EUR', 'GBP'],
+      required: true,
+      set: function(v){
+        this.internal.approximatePriceUSD = this.price.amount / (fx()[v] || 1);
+        return v;
+      }
+    }
+  },
+  category: Category.categorySchema,
+  internal: {
+    approximatePriceUSD: Number
+  }
+}
